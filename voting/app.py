e7d5fd3e33e0f8b58ed5fe04518111d428f7a1e5
saiganeshsunkari <saiganeshsunkari38@gmail.com>
2017-01-11 15:15:53 -0500

    First commit

--- voting/app.py
+++ voting/app.py
@@ -9,8 +9,8 @@ import logging
 import signal
 import sys
 
-optionA = "Dev"
-optionB = "Ops"
+optionA = "Cloud"
+optionB = "Infra"
 name = optionA + " VS " + optionB
 
 redis = Redis(host="redis", db=0)
